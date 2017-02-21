ed5b198e04b6a76f36987396dead3e4af09fc9a3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-20 18:35:45 -0800

    My script is clean and working, db clean

--- static/db_setup.sql
+++ static/db_setup.sql
@@ -6,7 +6,6 @@ CREATE or replace FUNCTION getGeocode (location text) RETURNS geoResult AS $$
     import json
 
 
-    plpy.notice(location) 
     query = plpy.prepare("SELECT city_id, lat, lon FROM cachedgeocodes WHERE city = $1", ["text"])
     recds = plpy.execute(query, [location])
     if recds.nrows() > 0:  # & recds.status() == SPI_OK_SELECT (Maybe is 5):
@@ -25,20 +24,28 @@ CREATE or REPLACE FUNCTION getGeoFromAPI (location text) RETURNS geoResult AS $$
     import os
     import json
 
-    address = '+'.join(location.split(' ')).rstrip('+') # incase we get extra + for the empty spaces
+    address = '+'.join(location.split(' ')).rstrip('+') # In case we get extra + for the empty spaces
     key = ''
 
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address, key)
 
-    response = urllib2.urlopen(url)   #TRY_CATCH HERE
+    # In case any error getting the location, I do nothing and return None instead of 0,0,0.
+    try:
+        response = urllib2.urlopen(url)
+    except urllib2.HTTPError:
+        return {'city_id':None, 'lat':None, 'lon':None}
+    except plpy.SPIError:
+        return {'city_id':None, 'lat':None, 'lon':None}
+
     jsonresponse = json.loads(response.read())
     if jsonresponse['status']  == 'OK':
         geocode = jsonresponse["results"][0]['geometry']['location']
-        # We might not need these two, since I am puting the original location.
-        #city = [x["long_name"] for x in jsonresponse['results'][0]["address_components"] if x["types"][0] == "locality"][0]
-        #state = [x["short_name"] for x in jsonresponse['results'][0]["address_components"] if x["types"][0] == "administrative_area_level_1"][0]
-        plpy.notice(jsonresponse) 
-        country = [x["long_name"] for x in jsonresponse['results'][0]["address_components"] if x["types"][0] == "country"][0]
+        # In case I want to filter the good addresses, I could learn to check the results in "types".
+        # type_of_place = [x["types"] for x in jsonresponse['results'][0]["address_components"] if "political" in x["types"]]
+        country = [x["long_name"] for x in jsonresponse['results'][0]["address_components"] if x["types"][0] == "country"]
+        # Maybe add if country = USA here, but I think this should be handled by an calling object.
+        plpy.notice(len(country)) 
+        country = country[0] if len(country) > 0 else location  
 
         # Change to COuntry instead of state, city to city_state ??
         query = plpy.prepare("INSERT INTO cachedgeocodes (city, state, lat, lon) VALUES ($1, $2, $3, $4) returning city_id, lat, lon", ["text", "text", "float", "float"])
@@ -48,7 +55,7 @@ CREATE or REPLACE FUNCTION getGeoFromAPI (location text) RETURNS geoResult AS $$
     elif jsonresponse['status']  == 'ZERO_RESULTS':
         return {'city_id':0, 'lat':0, 'lon':0}
 
-    # Anything else could be an network erro or database error.
+    # Anything else could be an network error or database error. I return None instead of 0,0,0
     return {'city_id':None, 'lat':None, 'lon':None}
 
 
--- static/update_geo.sql
+++ static/update_geo.sql
@@ -5,12 +5,15 @@ DECLARE
     r tweets%rowtype;
     recd geoResult%rowtype;
 BEGIN
-    FOR r in  select * from tweets where city_id = 1 limit 5
+    raise notice 'HELLO';
+    FOR r in  select * from tweets where city_id = 1 AND author_location <> ''
     LOOP
+        raise notice 'WTF';
         IF r.author_location != '' THEN
             raise notice '%', r.author_location;
             recd = getGeocode(r.author_location);
             raise notice '%', recd;
+            raise notice '***************************************';
             UPDATE tweets SET city_id = cast(recd.city_id as int), lat = recd.lat, lon = recd.lon
             WHERE tweet_id = r.tweet_id;
         END IF;
